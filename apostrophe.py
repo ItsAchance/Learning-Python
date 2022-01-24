bd08dc170dba9c8704cb80f1d46c3d6f2e0c5bde
Alexander Chance <thealexanderchance@gmail.com>
2022-01-24 11:23:37 +0100

    Added comments to full_name and apostrophe

--- apostrophe.py
+++ apostrophe.py
@@ -1,2 +1,4 @@
+#Using double quotes to be able to use apostrohpe
+
 message = "One of Pythons's strenghts is its diverse community."
 print(message)
--- full_name.py
+++ full_name.py
@@ -1,3 +1,5 @@
+#This print the first letter in first_name and last_name in capital
+
 first_name = "ada"
 last_name = "lovelace"
 full_name = f"{first_name} {last_name}"
