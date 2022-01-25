540bc9ee032f76bc08f47e6ee0ed2be8d7e4ee0c
Alexander Chance <thealexanderchance@gmail.com>
2022-01-25 08:08:14 +0100

    Added ch4 and looping programs in it

---
+++ ch4/magicans.py
@@ -0,0 +1,7 @@
+#Using a for loop with a list
+
+magicians = ["alice", "david", "carolina"]
+
+for magician in magicians:
+	print(f"Thats was a great trick {magician.title()}!")
+	print(f"I can't wait to see your next trick {magician.title()}\n")
