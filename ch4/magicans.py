a5ab13be948353a1292c38cff9f12b8145764733
Alexander Chance <thealexanderchance@gmail.com>
2022-01-27 08:26:01 +0100

    Added looping list programs

--- ch4/magicans.py
+++ ch4/magicans.py
@@ -5,3 +5,5 @@ magicians = ["alice", "david", "carolina"]
 for magician in magicians:
 	print(f"Thats was a great trick {magician.title()}!")
 	print(f"I can't wait to see your next trick {magician.title()}\n")
+
+print("Thank you, everyone. That was a great magic show!")
---
+++ ch4/squares.py
@@ -0,0 +1,15 @@
+# Looping a list and rasing the number to the power of 2
+
+#squares = []
+#for value in range(1, 11):
+#	square = value ** 2
+#	squares.append(square)
+
+#print(squares)
+
+# This way you save one line
+squares = []
+for value in range(1, 11):
+	squares.append(value ** 2)
+
+print(squares)
