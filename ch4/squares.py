0bc03a13aa065da1b94ba908aabc2ac437b83445
Alexander Chance <thealexanderchance@gmail.com>
2022-01-28 10:52:55 +0100

    Added digits.py and updatesw squares.py

--- ch4/squares.py
+++ ch4/squares.py
@@ -8,8 +8,12 @@
 #print(squares)
 
 # This way you save one line
-squares = []
-for value in range(1, 11):
-	squares.append(value ** 2)
+#squares = []
+#for value in range(1, 11):
+#	squares.append(value ** 2)
 
-print(squares)
+#print(squares)
+
+# This way you can do it all in two lines
+squares = [value**2 for value in range(1, 11)]
+print (squares)
