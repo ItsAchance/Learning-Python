88c4ccd3a8ea9af91e7a8fbfeb4b22a3efb7e706
Chance <thealexanderchance@gmail.com>
2022-04-13 20:35:22 +0200

    Changed firstname to test git on my new linux dist

--- ch1/full_name.py
+++ ch1/full_name.py
@@ -1,6 +1,6 @@
 #This print the first letter in first_name and last_name in capital
 
-first_name = "ada"
+first_name = "allan"
 last_name = "lovelace"
 full_name = f"{first_name} {last_name}"
 message = f"Hello, {full_name.title()}!"
