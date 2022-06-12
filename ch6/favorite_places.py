4d6cb89d48902733859795563430d048ba0b7df5
ItsAchance <alexander.chance@tutanota.com>
2022-06-12 21:39:04 +0200

    added code

--- ch6/favorite_places.py
+++ ch6/favorite_places.py
@@ -1,3 +1,10 @@
 favorite_places = {
-	
+	"ben" : ["stockholm, oslo"],
+	"beth" : ["london, glasgow, dublin"],
+	"jack" : ["toronto, ottawa"]
 }
+
+for person, city in favorite_places.items():
+	print(f"{person.title()}s favorite places are: ")
+	for town in city:
+		print(f"{town.title()}\n")
