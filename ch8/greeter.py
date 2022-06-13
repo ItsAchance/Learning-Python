8ac8dd965f052f4f28253680b76684b3977f4cbb
ItsAchance <alexander.chance@tutanota.com>
2022-06-13 22:44:26 +0200

    Started ch8

---
+++ ch8/greeter.py
@@ -0,0 +1,5 @@
+def greet_user(username):
+	"""Dispay a simple greeting."""
+	print(f"Yo {username.title()}!")
+
+greet_user("joe")
---
+++ ch8/message.py
@@ -0,0 +1,4 @@
+def dispay_message():
+	print("I am learning about function in this chapter")
+
+dispay_message()
